984bda2bb294b640a2741b743457c9fcde340a6c
Documenter.jl <documenter@juliadocs.github.io>
2024-05-15 21:02:12 +0000

    build based on f973f51

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ScalarConvexPrototype","category":"page"},{"location":"#ScalarConvexPrototype","page":"Home","title":"ScalarConvexPrototype","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ScalarConvexPrototype.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ScalarConvexPrototype]","category":"page"}]
+}
